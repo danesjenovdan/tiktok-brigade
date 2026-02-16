85bcf5c531ebdc53455e520a9044dea4d1d7dad1
Tomaž Kunst <tomazkunst@gmail.com>
2026-02-16 17:37:59 +0100

    fix django storages

--- tiktok_brigade/settings.py
+++ tiktok_brigade/settings.py
@@ -152,7 +152,15 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # DJANGO STORAGE SETTINGS
 if os.getenv("ENABLE_S3", False) == "True":
-    DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+    # Django 4.2+ storage configuration
+    STORAGES = {
+        "default": {
+            "BACKEND": "storages.backends.s3boto3.S3Boto3Storage",
+        },
+        "staticfiles": {
+            "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+        },
+    }
     AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID", "")
     AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY", "")
     AWS_STORAGE_BUCKET_NAME = os.getenv("AWS_STORAGE_BUCKET_NAME", "")
